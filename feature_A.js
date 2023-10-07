8d5acbe9a7a48b78cd804e0f6ed3685429276fcd
Koushik <koushik.d@turntechindia.co.in>
2023-10-07 17:22:10 +0530

    checkbox tick pop and push elements in arr

--- feature_A.js
+++ feature_A.js
@@ -1,1 +1,32 @@
-console.log('i am the featureA');
+let arr = [];
+const ping = (data) => {
+    const check = arr.some((ele) => {
+        return ele === data;
+    });
+    if (check) {
+        const filtered_arr = arr.filter((ele) => {
+            return ele !== data;
+        });
+        arr = filtered_arr;
+    } else {
+        arr.push(data);
+    }
+    console.log(arr);
+}
+
+
+// const checkbox = document.getElementById('checkbox');
+// checkbox.addEventListener('click', (event, data = 'Laptop') => {
+//     const check = arr.some((ele) => {
+//         return ele === data;
+//     });
+//     if (check) {
+//         const filtered_arr = arr.filter((ele) => {
+//             return ele !== data;
+//         });
+//         arr = filtered_arr;
+//     } else {
+//         arr.push(data);
+//     }
+//     console.log(arr);
+// })
